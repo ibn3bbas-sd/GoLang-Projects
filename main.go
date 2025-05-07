ed434ccb929d953f4779f3e30433bd68372c3e05
Ibn Abbas Mohammed Elhadi <ibn3bbas.mo7ammed.elhadi@gmail.com>
2025-05-07 15:51:16 +0300

    add info

--- main.go
+++ main.go
@@ -5,5 +5,17 @@ package main
 
 func main() {
 	// This is a simple Go program that prints "Hello, World!" to the console.
-	println("Hello, World! I'm Ibn Abbas")
+	println("Hello, World!")
+
+	// This is a simple Go program that prints "Hello, World!" to the console.
+	var age int = 36
+	var name string = "Ibn Abbas"
+	var role string = "DevOps Engineer"
+	var location string = "Saudi Arabia"
+	var company string = "Bank of Khartoum"
+	println(age)
+	println(name)
+	println(role)
+	println(location)
+	println(company)
 }
