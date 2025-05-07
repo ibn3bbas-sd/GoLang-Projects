70cb5856d6c49bf4f760816aa40ef5931524a4f5
Ibn Abbas Mohammed Elhadi <ibn3bbas.mo7ammed.elhadi@gmail.com>
2025-05-07 16:24:48 +0300

    second commit

--- main.go
+++ main.go
@@ -6,8 +6,6 @@ package main
 func main() {
 	// This is a simple Go program that prints "Hello, World!" to the console.
 	println("Hello, World!")
-
-	// This is a simple Go program that prints "Hello, World!" to the console.
 	var age int = 36
 	var name string = "Ibn Abbas"
 	var role string = "DevOps Engineer"
---
+++ varTypes.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+
+	var age int = 36
+	var name string = "Ibn Abbas"
+	var isMarried bool = true
+	var height float32 = 172.5
+
+	println(name)
+	println(age)
+	println(isMarried)
+	println(height)
+}
