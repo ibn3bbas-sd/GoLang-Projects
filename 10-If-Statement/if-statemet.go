8317c14c9f38d44a0622c1ad1edb9cf5ff549b80
Ibn Abbas Mohammed Elhadi <ibn3bbas.mo7ammed.elhadi@gmail.com>
2025-05-14 13:06:02 +0300

    done

--- 10-If-Statement/if-statemet.go
+++ 10-If-Statement/if-statemet.go
@@ -1,5 +1,11 @@
 package main
 
+// If statement is used to execute a block of code if a specified condition is true.
+// If the condition is false, the code block will not be executed.
+// The if statement can be used with else and else if statements to create multiple conditions.
+// The if statement can also be used with logical operators (&&, ||) to combine multiple conditions.
+// The if statement can be used with comparison operators (==, !=, >, <, >=, <=) to compare values.
+
 import (
 	"fmt"
 )
