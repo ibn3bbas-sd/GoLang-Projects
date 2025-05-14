dfab6ba9ece6cb191713ec95ce9d442977e054b6
Ibn Abbas Mohammed Elhadi <ibn3bbas.mo7ammed.elhadi@gmail.com>
2025-05-14 13:46:50 +0300

    done nested

--- 11-Nasted-If-Statement/nasted-if-statement.go
+++ 11-Nasted-If-Statement/nasted-if-statement.go
@@ -1,8 +1,23 @@
 package main
+
 import (
 	"fmt"
 )
+
 func main() {
-	
-	if 
+
+	if number := 10; number > 0 {
+		if number%2 == 0 {
+			fmt.Println("The number is positive and even.")
+		} else {
+			fmt.Println("The number is positive and odd.")
+		}
+	}
+	if number := -5; number < 0 {
+		if number%2 == 0 {
+			fmt.Println("The number is negative and even.")
+		} else {
+			fmt.Println("The number is negative and odd.")
+		}
+	}
 }
