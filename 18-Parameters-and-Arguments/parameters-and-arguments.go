1386aba45ac0d8be5b2c83c4d14ddf8c68b5cfb2
Ibn Abbas Mohammed Elhadi <ibn3bbas.mo7ammed.elhadi@gmail.com>
2025-05-18 18:34:30 +0300

    done parameters

---
+++ 18-Parameters-and-Arguments/parameters-and-arguments.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func add(x int, y int, z int) int {
+	return x + y + z
+}
+
+func maltiply(x int, y int, z int) int {
+	return x * y * z
+}
+
+func power(x float64, y float64) float64 {
+	return math.Pow(x, y)
+}
+
+func main() {
+
+	result := add(2, 3, 4)
+	fmt.Println("The sum is:", result)
+	// Function call
+
+	product := maltiply(2, 3, 4)
+	fmt.Println("The product is:", product)
+	// Function call with different arguments
+
+	end := power(2, 34)
+	fmt.Println("The power is:", end)
+	// Function call with different arguments
+}
