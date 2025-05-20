569732881f8e3338c904ecb71386cdc90825b688
Ibn Abbas Mohammed Elhadi <ibn3bbas.mo7ammed.elhadi@gmail.com>
2025-05-20 16:03:01 +0300

    done

--- 25-Comparison-and-Assignment-of-Structs/comparison-and-assignment-of-structs.go
+++ 25-Comparison-and-Assignment-of-Structs/comparison-and-assignment-of-structs.go
@@ -28,3 +28,8 @@ func main() {
 	person3 := person1
 	fmt.Println("person3:", person3)
 }
+
+// Comparison of structs: In Go, you can compare structs directly using the == operator.
+// This compares each field of the struct for equality.
+// However, if the struct contains fields that are not comparable (like slices or maps), you will get a compilation error.
+// To compare structs with non-comparable fields, you can use the reflect package to perform a deep comparison.
