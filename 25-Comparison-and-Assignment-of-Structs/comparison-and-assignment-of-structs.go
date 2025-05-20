1d6c6265c578433bd8a1c9f37f88c9fd540348b0
Ibn Abbas Mohammed Elhadi <ibn3bbas.mo7ammed.elhadi@gmail.com>
2025-05-20 14:51:29 +0300

    done comparison

---
+++ 25-Comparison-and-Assignment-of-Structs/comparison-and-assignment-of-structs.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type Person struct {
+	Name    string
+	Age     int
+	Hobbies []string
+}
+
+func areEqual(p1, p2 Person) bool {
+	return p1.Name == p2.Name && p1.Age == p2.Age && reflect.DeepEqual(p1.Hobbies, p2.Hobbies)
+}
+
+func main() {
+	person1 := Person{Name: "Sawda", Age: 3, Hobbies: []string{"Playing", "Chatting"}}
+	person2 := Person{Name: "Ihsan", Age: 28, Hobbies: []string{"Reading", "Traveling"}}
+
+	if areEqual(person1, person2) {
+		fmt.Println("person1 and person2 are equal")
+	} else {
+		fmt.Println("person1 and person2 are not equal")
+	}
+
+	person3 := person1
+	fmt.Println("person3:", person3)
+}
